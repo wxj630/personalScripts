63ee7bfe4e6dcfccf224089f8a4a8b868f693164
wuxiaojun <717366564@qq.com>
2020-07-21 10:03:15 +0800

    add author config info

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: wuxiaojun
+
+@contact: wuxiaojun@cetccity.com
+
+@software: PyCharm
+
+@froject name: personalScripts
+
+@file: hello_world.py
+
+@time: 2020/7/21  10:01 
+
+@desc: hello world example
+"""
+
+print('hello world!')
